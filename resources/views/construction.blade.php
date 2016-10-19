0a062cc56564ab8702218a9462173664aae848eb
Jasna Shamz <Jasna Shamz>
2016-10-19 15:54:43 +0530

    commit

---
+++ resources/views/construction.blade.php
@@ -0,0 +1,14 @@
+
+@extends('layouts.layout')
+
+@section('title')
+    Under Construction
+@stop
+
+@section('body')
+<h1>Page Under Construction </h1>
+<h3>This page is under construction. Please come back soon!  </h3>
+@stop
+
+
+
