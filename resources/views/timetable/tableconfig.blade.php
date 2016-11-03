77e5c523722d8d4e2411382659c646c71ffcab52
Akhil-Krishnan <akhilkrishnan060@gmail.com>
2016-11-03 14:12:10 +0530

    config

--- resources/views/timetable/tableconfig.blade.php
+++ resources/views/timetable/tableconfig.blade.php
@@ -13,15 +13,25 @@
       <div class="box box-success">
 	      <div class="box-body">
 				<table id="example2" border='1' class="table table-bordered table-hover">
-        <div class="form-group">
+        <br/><div class="form-group">
+		<div class="col-md-4">
         {!! Form::label('day', 'No of working days in a week') !!}
+		</div>
+		<div class="col-md-8">
         {!! Form::text('no_of_days_week', null, ['class' => 'form-control', 'placeholder'=>'']) !!}
+		</div>
+		<br/><br/>
         </div>
          <div class="form-group">
+		 <div class="col-md-4">
         {!! Form::label('hour', 'No of working hours in a day') !!}
+		</div>
+		<div class="col-md-8">
        {!! Form::text('no_of_hours_day', null, ['class' => 'form-control', 'placeholder'=>'']) !!}
+	   </div>
         </div>
-          <div class="form-group">
+		<br/><br/>
+        <div class="form-group" style="text-align:center">
             {!! Form::submit( 'Save', ['class'=>'btn btn-primary']) !!} 
         </div>
 
